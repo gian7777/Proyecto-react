c3686961cf7a53b486ba847a7834048b1f7ae514
Gian Decon <giandecon@gmail.com>
2022-07-11 20:22:30 -0300

    Clase2 + Decon

---
+++ src/componentes/navBar.js
@@ -0,0 +1,14 @@
+function navs() {
+    return (
+        
+        <nav>
+            <ul>
+              <li> <a href="./index.html">Productos</a> </li>
+              <li> <a href="./index.html">Tiendas</a> </li>
+              <li> <a href="./index.html">Nosotros</a> </li>
+            </ul>
+        </nav>
+        
+    ); 
+    }
+export default navs
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import navs from './componentes/navBar';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
